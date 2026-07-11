17727a872934dccb9960ada4256a1bf43ba22d64
Albina2503 <albinabahtiguzina1@gmail.com>
2026-07-11 17:49:39 +0300

    Update Colony.h

--- Colony.h
+++ Colony.h
@@ -3,28 +3,17 @@
 #include "TransportRoute.h"
 #include <memory>
 #include <random>
-#include <vector>
 using namespace std;
 
 class Colony{
 private:
     vector<shared_ptr<ColonyModule>> modules;
     vector<shared_ptr<TransportRoute>> routes;
-    mt19937 rng;  // генератор случайных чисел
+    mt19937 rng;  // генератор случайных чисел 
 public:
     Colony();
-    void createModules(
-        int habitatCount,
-        int greenhouseCount,
-        int solarCount,
-        int nuclearCount,
-        int mineCount,
-        int waterRecyclerCount,
-        int storageCount,
-        int medicalCount,
-        int repairBayCount
-    );
-    void createTransportNetwork(int totalRoutes);
+    void createModules();
+    void createTransportNetwork(int totalRoutes); 
     const vector<shared_ptr<ColonyModule>>& getModules() const { return modules; }
     const vector<shared_ptr<TransportRoute>>& getRoutes() const { return routes; }
 };
