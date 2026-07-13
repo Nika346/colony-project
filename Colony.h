bdc88137c4593e271fab5c3b51191ceb8f178e17
Emptymess121 <gudimovaaksina7@gmail.com>
2026-07-13 14:40:39 +0300

    Update Colony.h
    доп задания 12,13 и 14

--- Colony.h
+++ Colony.h
@@ -16,4 +16,10 @@ public:
     void createTransportNetwork(int totalRoutes); 
     const vector<shared_ptr<ColonyModule>>& getModules() const { return modules; }
     const vector<shared_ptr<TransportRoute>>& getRoutes() const { return routes; }
+
+    // доп задания 12,13,14
+    void save(const string& file_name) const;
+    void download(const string& file_name);
+    void export_csv(const string& file_name) const;
+    void export_dot(const string& file_name) const;
 };
