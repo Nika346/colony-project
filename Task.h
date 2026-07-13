cef5adb8a0f5e9eb704f1b45a7070eab3de1fdff
Albina2503 <albinabahtiguzina1@gmail.com>
2026-07-13 19:54:20 +0300

    Create Task.h

---
+++ Task.h
@@ -0,0 +1,33 @@
+class Task {
+private:
+    Task_type type;
+    string nameTarget;               // название места в котором нужно выполнить задачу
+    variant<monostate, ColonyModule*, TransportRoute* > target;     // автоматически определяется тип места(модуль или путь)
+    int complexity;                 // сложность/время выполнения
+    double priority;                // рассчитанный приоритет
+    string status;                  // статус задачи
+    variant<monostate, Robot*, ColonistGroup* > assigned;         // указатель на робота или группу колонистов
+
+public:
+    Task(Task_type t, ColonyModule* target, int moduleImportance, 
+         double damageLevel, int complexity = 1);
+     Task(Task_type t, TransportRoute* targetRoute, int importance, 
+         double damageLevel, int complexity = 1);
+    double getPriority() const { return priority; }
+    Task_type getType() const { return type; }
+    string getNameTarget() const { return nameTarget; }
+    int getComplexity() const { return complexity; }
+    string getStatus() const { return status; }
+
+    ColonyModule* getTargetModule() const;
+    TransportRoute* getTargetRoute() const;
+
+    // Назначение исполнителя
+    void assignTo(Robot* robot);
+    void assignTo(ColonistGroup* group);
+
+    Robot* getAssignedRobot() const;
+    ColonistGroup* getAssignedColonistGroup() const;
+    bool isAssigned() const;
+    void setStatus(const string& newStatus);
+    }; 
